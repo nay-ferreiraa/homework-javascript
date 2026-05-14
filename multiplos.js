47b97676e9872de3822ebcb90e6687c69d08bb25
nay-ferreiraa <nay.ferreirasilvaa@gmail.com>
2026-05-14 16:46:05 -0300

    Add files via upload

---
+++ multiplos.js
@@ -0,0 +1,13 @@
+function somarMultiplos() {
+  let soma = 0;
+
+  for (let i = 1; i < 1000; i++) {
+    if (i % 5 === 0 || i % 7 === 0) {
+      soma += i;
+    }
+  }
+
+  return soma;
+}
+
+console.log(somarMultiplos());
